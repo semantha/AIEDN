2119d59b9a42ab5f8a7eb3263b7e81bbc47918e6
Niko <niko.duerr@icloud.com>
2023-02-21 15:30:10 +0100

    Select explanation video according to group

--- page_views/video_page.py
+++ page_views/video_page.py
@@ -2,15 +2,19 @@ import time
 from page_views.abstract_page import AbstractPage
 import streamlit as st
 
+CONTROL_GROUP_VIDEO = "https://www.youtube.com/watch?v=NseWRQ1JYoM"
+EXPERIMENTAL_GROUP_VIDEO = "https://www.youtube.com/watch?v=7Twnmhe948A"
+
 
 class VideoPage(AbstractPage):
     def __init__(self, page_id, page_manager):
         super().__init__(page_id)
         self.page_manager = page_manager
+        self.video_url = self.choose_video()
 
     def display_page(self):
         st.success("🕵🏻 Vielen Dank, wir zeigen dir nun ein kurzes Erklärvideo.")
-        st.video("https://www.youtube.com/watch?v=NseWRQ1JYoM")
+        st.video(self.video_url)
         _, _, col, _, _ = st.columns(5)
         time.sleep(5)
         col.button(
@@ -19,3 +23,9 @@ class VideoPage(AbstractPage):
             key="next_button_video_not_done",
             help="Klicke hier um zur nächsten Seite zu gelangen.",
         )
+
+    @staticmethod
+    def choose_video():
+        if st.session_state.control:
+            return CONTROL_GROUP_VIDEO
+        return EXPERIMENTAL_GROUP_VIDEO

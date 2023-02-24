4982f10f31b8bef8205fa8711cab78b4a94c438c
Niko <niko.duerr@icloud.com>
2023-02-24 17:10:41 +0100

    Fix duplicates issue

--- page_views/search_page.py
+++ page_views/search_page.py
@@ -75,7 +75,6 @@ class SearchPage(AbstractPage):
                 self.__match_handling(search_string, results)
 
     def __match_handling(self, search_string, results):
-        results = self.__remove_duplicates(results)
         video_string = "passendes Video" if len(results) == 1 else "passende Videos"
         st.success(
             f"Erledigt! Ich habe **{len(results)}** {video_string} für dich gefunden!",
@@ -85,24 +84,33 @@ class SearchPage(AbstractPage):
             self.__semantha.add_to_library(
                 content=search_string, tag=st.session_state.user_id
             )
-
+        results = self.__handle_duplicates(results)
         if not self.__sidebar.get_show_videos_below_each_other():
             st.session_state["tabs"] = ["Video #1"]
-            for i, row in results.iterrows():
+            for i, _ in results.iterrows():
                 if i >= 2:
                     st.session_state["tabs"].append(f"Video #{i}")
 
             tabs = st.tabs(st.session_state["tabs"])
-        for i, row in results.iterrows():
-            if self.__sidebar.get_show_videos_below_each_other():
-                self.__display_results_below_each_other(results, i, row)
-            else:
-                self.__display_result_in_tabs(results, i, row, tabs)
+
+        if self.__sidebar.get_show_videos_below_each_other():
+            self.__display_results_below_each_other(results)
+        else:
+            self.__display_result_in_tabs(results, tabs)
         if self.__sidebar.get_debug():
             self.__debug_view(results)
 
-    def __remove_duplicates(self, results):
-        return results.drop_duplicates(subset=["video_id", "start"], keep="first")
+    def __handle_duplicates(self, results):
+        new_results = {}
+        present = {}
+        for i, row in results.iterrows():
+            video_id, start, category = self.__extract_metadata_info(results, i, row)
+            if (video_id, start) in present:
+                new_results[present[(video_id, start)]]["tags"] += ", " + category
+            else:
+                new_results[i] = row
+                present[(video_id, start)] = i
+        return new_results
 
     def __debug_view(self, results):
         with st.expander("Ergebnisse", expanded=False):
@@ -118,24 +126,29 @@ class SearchPage(AbstractPage):
                 content=search_string, tag=st.session_state.user_id + ",no_match"
             )
 
-    def __display_result_in_tabs(self, results, i, row, tabs):
-        video_id, start, content, category, video = self.__get_result_info(
-            results, i, row
-        )
-        with tabs[i - 1]:
-            self.__display_video(video_id, start, content, category, video)
-
-    def __display_results_below_each_other(self, results, i, row):
-        video_id, start, content, category, video = self.__get_result_info(
-            results, i, row
-        )
-        st.subheader(f"Video {i} von {len(results)}")
-        self.__display_video(video_id, start, content, category, video)
-        if i >= 1 and i < len(results):
-            st.markdown(
-                """<hr style="height:2px;border:none;color:#333;background-color:#333;" /> """,
-                unsafe_allow_html=True,
+    def __display_result_in_tabs(self, results, tabs):
+        for i, row in results.iterrows():
+            video_id, start, content, category, video = self.__get_result_info(
+                results, i, row
             )
+            with tabs[i - 1]:
+                self.__display_video(video_id, start, content, category, video)
+
+    def __display_results_below_each_other(self, results):
+        for i, row in results.iterrows():
+            st.subheader(f"Video {i} von {len(results)}")
+            video_id, start, content, category, video = self.__get_result_info(
+                results, i, row
+            )
+            self.__display_video(video_id, start, content, category, video)
+            if i >= 1 and i < len(results):
+                self.__display_horizontal_line()
+
+    def __display_horizontal_line(self):
+        st.markdown(
+            """<hr style="height:2px;border:none;color:#333;background-color:#333;" /> """,
+            unsafe_allow_html=True,
+        )
 
     def __display_video(self, video_id, start, content, category, video):
         if not st.session_state.control:
@@ -145,12 +158,19 @@ class SearchPage(AbstractPage):
         st.markdown(f"📺 **Video:** _{video}_")
 
     def __get_result_info(self, results, i, row):
+        video_id, start, content, category = self.__extract_metadata_info(
+            results, i, row
+        )
+        content = results.at[i, "Content"]
+        video = results.at[i, "Name"].split("_")[0]
+        return video_id, start, content, category, video
+
+    def __extract_metadata_info(self, results, i, row):
         results.at[i, "Metadata"] = ast.literal_eval(row["Metadata"])
         video_id = results.at[i, "Metadata"]["id"]
         start = 0 if st.session_state.control else results.at[i, "Metadata"]["start"]
-        content = results.at[i, "Content"]
+
         category = results.at[i, "Tags"]
         category = [tag for tag in category if tag not in ["base", "11"]]
         category = ", ".join(category)
-        video = results.at[i, "Name"].split("_")[0]
-        return video_id, start, content, category, video
+        return video_id, start, category

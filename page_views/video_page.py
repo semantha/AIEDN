051cc215306ba8af7a6ad473c1cddeef449455cb
Sebastian Weigelt <sebastian.weigelt@thingsthinking.net>
2023-03-02 10:47:48 +0100

    TP-6009: remove timeout stuff for intro videos

--- page_views/video_page.py
+++ page_views/video_page.py
@@ -1,5 +1,3 @@
-import time
-
 import streamlit as st
 from streamlit_player import st_player
 
@@ -7,20 +5,17 @@ from page_views.abstract_page import AbstractPage
 
 _CONTROL_GROUP_VIDEO = "https://vimeo.com/803009708"
 _EXPERIMENTAL_GROUP_VIDEO = "https://vimeo.com/803009422"
-_EXPERIMENTAL_GROUP_VIDEO_LENGTH = 80
-_CONTROL_GROUP_VIDEO_LENGTH = 75
 
 
 class VideoPage(AbstractPage):
-    def __init__(self, page_manager, sidebar):
+    def __init__(self, page_manager):
         self.__page_manager = page_manager
-        self.__sidebar = sidebar
         if "video_first_time" not in st.session_state:
             st.session_state.video_first_time = True
 
     def display_page(self):
         st.success("🕵🏻 Vielen Dank, wir zeigen dir nun ein kurzes Erklärvideo.")
-        video_url, video_length = self.__choose_video()
+        video_url = self.__choose_video()
         st_player(video_url, height=400, playing=True, config={
             "vimeo": {
                 "playerOptions": {
@@ -30,9 +25,6 @@ class VideoPage(AbstractPage):
             }
         })
         _, _, col, _, _ = st.columns(5)
-        if st.session_state.video_first_time and not self.__sidebar.get_video_skippable():
-            time.sleep(video_length)
-        st.session_state.video_first_time = False
         col.button(
             "⏭️ Weiter",
             on_click=self.__page_manager.nextpage,
@@ -43,5 +35,5 @@ class VideoPage(AbstractPage):
     @staticmethod
     def __choose_video():
         if st.session_state.control:
-            return _CONTROL_GROUP_VIDEO, _CONTROL_GROUP_VIDEO_LENGTH
-        return _EXPERIMENTAL_GROUP_VIDEO, _EXPERIMENTAL_GROUP_VIDEO_LENGTH
+            return _CONTROL_GROUP_VIDEO
+        return _EXPERIMENTAL_GROUP_VIDEO
